33d42b04acd4ef261e62f103b9e1c9852d91c4a4
Carlos Pons <karlospn@gmail.com>
2017-04-01 19:34:51 +0200

    Implemented CRUD operations on WebAPI

--- cats_mongodb.js
+++ cats_mongodb.js
@@ -1,18 +1,61 @@
 var _ = require('lodash');
-var Cats = require('./mongoose_models/cat_model');
+var Cat = require('./mongoose_models/cat_model');
 
 module.exports = function(app){
 
     app.get('/cats', function(req, res){
-        Cats.find({}, function(err, cats){
-            if(err){
+        Cat.find({}, function(err, cats){
+            if(err) 
                 res.sendStatus(400);
-            }
-            else{
-                res.send(cats);
-            }
+            else  
+                res.send(cats); 
         });
     });
 
-    app.get('/ca')
+     app.get('/cats/:id', function(req, res){
+        Cat.findById(req.params.id, function(err, cat){
+            if(err) 
+                res.sendStatus(400);
+            else  
+                res.send(cat); 
+        });
+    });
+
+    app.post('/cats', function(req, res){
+        var newCat = Cat(req.body);
+        newCat.save(function(err) {
+            if (err)
+                res.sendStatus(400);
+            else
+                res.send(newCat);
+        });
+    });
+
+    app.put('/cats/:id', function(req, res){
+        Cat.findById(req.params.id, function(err, cat){
+            if(err)
+                res.json({info : 'error finding cat', error: err});
+            else
+                if(cat){
+                    _.merge(cat, req.body);
+                    cat.save(function(err){
+                        if(err)
+                            res.json({info : 'error updating cat', error: err});
+                        else
+                            res.send(cat);
+                    });
+                }
+                else
+                    res.json({info : 'error cat not found', error: err});
+        });
+    });
+
+    app.delete('/cats/:id', function(req, res){
+        Cat.findByIdAndRemove(req.params.id, function(err, cat){
+            if(err) 
+                res.sendStatus(400);
+            else  
+                res.send('Cat deleted succesfully'); 
+        });
+    });
 }
